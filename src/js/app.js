02b012ae538adb0c960b9aa2843709982e7073f9
Alejandro Tellez <alejo082495@gmail.com>
2022-09-08 11:02:10 -0500

    Se eliminan algunos espacios de línea en el código HTML, se integra con JavaScript barra fija solo para dispositivos con una resolución de pantalla mayor o igual a 992 px en caso contrario no debe aparecer.

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,25 @@
-window.addEventListener('load', function() {
-    cambiarPlaceHolderDate1();
-    cambiarPlaceHolderDate2();
-});
+document.addEventListener('DOMContentLoaded', function() {
+    placeholderFecha();
+    navegacionFija();
+})
+
+function placeholderFecha() {
+    window.addEventListener('load', function() {
+        cambiarPlaceHolderDate1();
+        cambiarPlaceHolderDate2();
+    });
+}
 
 function cambiarPlaceHolderDate1() {
     const fechainicio = document.querySelector('#fechainicio');
     fechainicio.type = Text;
 
     fechainicio.addEventListener('blur', function() {
-
         fechainicio.type = 'text';
-
     })
 
     fechainicio.addEventListener('focus', function() {
         fechainicio.type = 'date';
-
     });
 }
 
@@ -24,13 +28,23 @@ function cambiarPlaceHolderDate2() {
     fechafinal.type = Text;
 
     fechafinal.addEventListener('blur', function() {
-
         fechafinal.type = 'text';
-
     })
 
     fechafinal.addEventListener('focus', function() {
         fechafinal.type = 'date';
-
     });
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const banner = document.querySelector('.banner');
+
+    window.addEventListener('scroll', () => {
+        if (banner.getBoundingClientRect().top < 0) {
+            barra.classList.add('fijo');
+        } else {
+            barra.classList.remove('fijo');
+        }
+    })
 }
