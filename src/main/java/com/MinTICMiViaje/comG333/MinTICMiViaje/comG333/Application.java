78ec2696bd850616e643e7683047ca170b643648
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2022-09-13 17:58:01 -0500

    inicializacion del proyecto

---
+++ src/main/java/com/MinTICMiViaje/comG333/MinTICMiViaje/comG333/Application.java
@@ -0,0 +1,13 @@
+package com.MinTICMiViaje.comG333.MinTICMiViaje.comG333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/MinTICMiViaje/comG333/MinTICMiViaje/comG333/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MinTICMiViaje.comG333.MinTICMiViaje.comG333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
